2d6f4c44e0c6bf75c69b2f4c531986028bf8268e
Pramudya76 <pramudyarama76@gmail.com>
2025-03-16 04:39:06 +0700

    Mem-Fix script stick mode

--- Assets/Script/StickyMode.cs
+++ Assets/Script/StickyMode.cs
@@ -5,15 +5,15 @@ using UnityEngine;
 public class StickyMode : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private LayerMask wallLayer;
-    private LayerMask ceilingLayer;
-    private LayerMask groundLayer;
+    private LayerMask TargetLayer;
+    // private LayerMask ceilingLayer;
+    // private LayerMask groundLayer;
     // Start is called before the first frame update
     void Start()
     {
-        wallLayer = LayerMask.GetMask("Walls");
-        ceilingLayer = LayerMask.GetMask("ceilingLayer");
-        groundLayer = LayerMask.GetMask("Ground");
+        TargetLayer = LayerMask.GetMask("Walls", "Ground","ceilingLayer");
+        // ceilingLayer = LayerMask.GetMask("ceilingLayer");
+        // groundLayer = LayerMask.GetMask("Ground");
         rb = GetComponent<Rigidbody2D>();
     }
 
@@ -23,13 +23,13 @@ public class StickyMode : MonoBehaviour
         float moveX = Input.GetAxis("Horizontal");
         //rb.velocity = new Vector2(moveX * 3, rb.velocity.y);
         
-        float distance = 0.66f;
-        RaycastHit2D HitLeft = Physics2D.Raycast(transform.position, Vector2.left, distance, wallLayer);
-        RaycastHit2D HitRight = Physics2D.Raycast(transform.position, Vector2.right, distance, wallLayer);
-        RaycastHit2D HitDown = Physics2D.Raycast(transform.position, Vector2.down, distance, groundLayer);
-        RaycastHit2D HitUp = Physics2D.Raycast(transform.position, Vector2.up, distance, wallLayer);
+        float distance = 0.69f;
+        RaycastHit2D HitLeft = Physics2D.Raycast(transform.position, Vector2.left, distance, TargetLayer);
+        RaycastHit2D HitRight = Physics2D.Raycast(transform.position, Vector2.right, distance, TargetLayer);
+        RaycastHit2D HitDown = Physics2D.Raycast(transform.position, Vector2.down, distance, TargetLayer);
+        RaycastHit2D HitUp = Physics2D.Raycast(transform.position, Vector2.up, distance, TargetLayer);
         
-        if(HitDown.collider != null && HitLeft.collider == null) {
+        if(HitDown.collider != null && HitLeft.collider == null && HitRight.collider == null) {
            rb.velocity = new Vector2(moveX * 3, rb.velocity.y);
            rb.gravityScale = 1;
         }else if(HitLeft.collider != null && HitUp.collider ==  null) {
@@ -38,7 +38,7 @@ public class StickyMode : MonoBehaviour
         }else if(HitUp.collider != null && HitLeft.collider == null && HitRight.collider == null) {
             rb.velocity = new Vector2(moveX * 3, 1f);
             rb.gravityScale = -1;
-        }else if(HitRight.collider != null && HitUp.collider == null) {
+        }else if(HitRight.collider != null && HitUp.collider == null ) {
             rb.velocity = new Vector2(1f, moveX * 3);
             rb.gravityScale = 0;
         }else if(HitDown.collider == null && HitLeft.collider == null && HitUp.collider == null && HitRight.collider == null) {
@@ -70,6 +70,14 @@ public class StickyMode : MonoBehaviour
                 rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y - 2);
             }else if(Input.GetKey(KeyCode.A)) {
                 rb.velocity = new Vector2(rb.velocity.x - 2, rb.velocity.y);
+            }
+        }
+
+        if(HitDown.collider != null && HitRight.collider != null) {
+            if(Input.GetKey(KeyCode.A)) {
+                rb.velocity = new Vector2(rb.velocity.x - 2, rb.velocity.y);
+            }else if(Input.GetKey(KeyCode.D)) {
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + 2);
             }
         }
         
