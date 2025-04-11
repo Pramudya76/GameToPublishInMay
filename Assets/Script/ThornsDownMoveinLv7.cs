f70f41d8c3a7de7db5ddcccc22ab83ed32ba89e7
Pramudya76 <pramudyarama76@gmail.com>
2025-04-11 16:29:20 +0700

    Progress Lv7 (75%)

---
+++ Assets/Script/ThornsDownMoveinLv7.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThornsDownMoveinLv7 : MonoBehaviour
+{
+    public float moveDistance;
+    private float moveSpeed = 0.5f;
+    private float startY;
+    private bool move = false;
+    private bool moving = true;
+    private Vector2 targetPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startY = transform.position.y;
+        targetPosition = new Vector2(transform.position.x, startY + moveDistance);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(move) return;
+        
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        
+        if(Vector2.Distance(transform.position, targetPosition) <= 0.1f) {
+            StartCoroutine(CDStay());
+        }
+    }
+
+    IEnumerator CDStay() {
+        move = true;
+        yield return new WaitForSeconds(2f);
+
+        moving = !moving;
+        float newY = moving ? startY + moveDistance : startY;
+        targetPosition = new Vector2(transform.position.x, newY);
+
+        move = false;
+   }
+
+}
---
+++ Assets/Script/ThornsUpMoveinLv7.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThornsUpMoveinLv7 : MonoBehaviour
+{
+    private float moveDistance = 0.8f;
+    private float moveSpeed = 0.5f;
+    private float startY;
+    private bool move = false;
+    private bool moving = true;
+    private Vector2 targetPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startY = transform.position.y;
+        targetPosition = new Vector2(transform.position.x, startY + moveDistance);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // float newY = startY + Mathf.PingPong(Time.time * moveSpeed, moveDistance);
+        // transform.position = new Vector2(transform.position.x, newY);
+
+        if(move) return;
+        
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+        
+        if(Vector2.Distance(transform.position, targetPosition) <= 0.1f) {
+            StartCoroutine(CDStay());
+        }
+
+
+    }
+
+   IEnumerator CDStay() {
+        move = true;
+        yield return new WaitForSeconds(2f);
+
+        moving = !moving;
+        float newY = moving ? startY + moveDistance : startY;
+        targetPosition = new Vector2(transform.position.x, newY);
+
+        move = false;
+   }
+
+
+
+}
